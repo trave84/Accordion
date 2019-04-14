9b47878aa872c4948b4296121c58d3f68c77919f
trave84 <marketerheaven@gmail.com>
2019-04-14 18:37:39 +0200

    JS works. Need to add more styling animation. Caret and transitioning

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Event handling...
+let title = document.getElementById("title-btn");
+let text = document.getElementById("text-content");
+
+title.onclick = () => {
+  if (text.style.maxHeight) {
+    text.style.maxHeight = null;
+  } else {
+    text.style.maxHeight = text.scrollHeight + "px";
+  }
+};
