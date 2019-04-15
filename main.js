272aa2551c9ff7fe90ff2860edf86610471356ee
trave84 <marketerheaven@gmail.com>
2019-04-15 03:13:57 +0200

    CSS styling done

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let title = document.getElementById("title-btn");
 let text = document.getElementById("text-content");
+let access = title.getAttribute("aria-expanded");
 
 title.onclick = () => {
+  title.classList.toggle("text-shown");
   if (text.style.maxHeight) {
     text.style.maxHeight = null;
+    access = "false";
   } else {
     text.style.maxHeight = text.scrollHeight + "px";
+    access = "true";
   }
+  title.setAttribute("aria-expanded", access);
+  text.setAttribute("aria-hidden", !access);
 };
