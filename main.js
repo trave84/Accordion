5877e0aa6b9b9a2ce6999c4155720d9448ace97f
trave84 <marketerheaven@gmail.com>
2019-04-15 03:21:13 +0200

    Aria in JS

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 let title = document.getElementById("title-btn");
 let text = document.getElementById("text-content");
-let access = title.getAttribute("aria-expanded");
+let expansion = title.getAttribute("aria-expanded");
+let visibility = text.getAttribute("aria-hidden");
 
 title.onclick = () => {
   title.classList.toggle("text-shown");
   if (text.style.maxHeight) {
     text.style.maxHeight = null;
-    access = "false";
+    expansion = "false";
+    visibility = true;
   } else {
     text.style.maxHeight = text.scrollHeight + "px";
     access = "true";
+    visibility = false;
   }
   title.setAttribute("aria-expanded", access);
-  text.setAttribute("aria-hidden", !access);
+  text.setAttribute("aria-hidden", visibility);
 };
